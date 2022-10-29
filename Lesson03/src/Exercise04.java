b11e78f4cadb647f0e23b079d43d1cddc1b206ee
nguyenntt <nt.thaonguyen25@gmail.com>
2022-10-29 18:31:33 +0700

    Fix exercise 4 (code on the same Method)

--- Lesson03/src/Exercise04.java
+++ Lesson03/src/Exercise04.java
@@ -1,38 +1,33 @@
 import java.util.Scanner;
 
-//Bài 4: Viết chương trình liệt kê n số nguyên tố đầu tiên.
-
 public class Exercise04 {
     public static void main(String[] args) {
-        System.out.println("Nhập số n: ");
+
+        //Bài 4: Viết chương trình liệt kê n số nguyên tố đầu tiên.
+
+        System.out.print("Nhập vào n: ");
         int n = new Scanner(System.in).nextInt();
-        int i = 2;
+
+        boolean check = true;
+        int num = 2;
         int count = 0;
 
-        //Thiết lập đếm và trả giá trị n nếu n là SNT
-        while (count <= n) {
-            if (check(i)) {
-                System.out.print(i + "\t");
-                count++;
-            }
-            i++;
-        }
-    }
+        System.out.println(n + " Số nguyên tố đầu tiên là:");
 
-    //Kiểm tra xem i có phải số NT không?
-    public static boolean check(int n) {
-        if (n < 2) {
-            return false;
-        } else {
-            for (int i = 2; i <= Math.sqrt(n); i++) {
-                if (n % i == 0) {
-                    return false;
+        while(count<n) {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    check = false;
+                    break;
                 }
             }
+            if (check) {
+                System.out.print(num+"\t");
+                count++;
+            }
+            check = true;
+            num++;
         }
-        return true;
     }
 }
-
-
 
