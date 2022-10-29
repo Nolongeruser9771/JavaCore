eababa923becbdf19c935cb49e32b86b1efb9e60
nguyenntt <nt.thaonguyen25@gmail.com>
2022-10-29 19:41:53 +0700

    Fix exercise 5,6 (after refer your instruction))

--- Lesson03/src/Exercise05.java
+++ Lesson03/src/Exercise05.java
@@ -5,20 +5,20 @@ public class Exercise05 {
         /* Bài 5: Một số được gọi là số thuận nghịch nếu ta đọc từ trái sang phải hay từ phải sang trái số đó ta vẫn
          nhận được một số giống nhau. Hãy nhập một số và kiểm tra xem số đó có phải số thuật nghịch hay không (ví dụ số: 558855).*/
 
-        System.out.print("Nhập 1 số n để kiểm tra: ");
-        String n= new Scanner(System.in).nextLine();
-        int i=String.valueOf(n).length();
-        String nreverse = "";
+        System.out.print("Nhập 1 số n có ít nhất 2 chữ số để kiểm tra: ");
+        long n = new Scanner(System.in).nextLong();
+        long tempt = n;
+        long Number = 0;
 
-        for (; i >0 ; i--) {
-           nreverse+=n.charAt(i-1);
+        while (tempt > 0) {
+            Number = Number*10 + tempt%10;
+            tempt/=10;
         }
-            int a=Integer.valueOf(nreverse);
-            int b=Integer.valueOf(n);
-            if(a==b) {
-                System.out.println(n+" là số thuận nghịch");
-            } else {
-                System.out.println(n+" không là số thuận nghịch");
-            }
+
+        if(Number==n) {
+            System.out.println(n+" là số thuận nghịch");
+        } else {
+            System.out.println(n+" không là số thuận nghịch");
+        }
     }
 }
--- Lesson03/src/Exercise06.java
+++ Lesson03/src/Exercise06.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Exercise06 {
     public static void main(String[] args) {
         //Bài 6: Hãy viết chương trình tính tổng các chữ số của một số nguyên bất kỳ. Ví dụ: Số 8545604 có tổng các chữ số là: 8+5+4+5+6+0+4=32
-        System.out.print("Nhập một số nguyên bất kì: ");
-        String n= new Scanner(System.in).nextLine();
-        int i=0;
-        String charIndex;
-        int Sum=0;
 
-        for (; i<String.valueOf(n).length() ; i++) {
-            charIndex= String.valueOf(n.charAt(i));
-            Sum+=Integer.valueOf(charIndex);
+        System.out.print("Nhập một số nguyên bất kì: ");
+        long n = new Scanner(System.in).nextLong();
+        long tempt = n;
+        long Sum = 0;
+
+        while (tempt > 0) {
+            Sum += tempt%10;
+            tempt/=10;
         }
         System.out.println("Tổng các chữ số là: "+Sum);
     }
