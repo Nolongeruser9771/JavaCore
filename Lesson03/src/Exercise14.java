f282caa44ce2c142a135f861b8499607f73405b0
nguyenntt <nt.thaonguyen25@gmail.com>
2022-10-29 20:48:19 +0700

    Wrong in reading the question

--- Lesson03/src/Exercise14.java
+++ Lesson03/src/Exercise14.java
@@ -5,19 +5,18 @@ public class Exercise14 {
         //Bài 14: Nhập số tự nhiên n rồi in ra các số chẵn nhỏ hơn n và các số lẻ nhỏ hơn n.
         System.out.print("Nhập số tự nhiên n: ");
         int n = new Scanner(System.in).nextInt();
-        int i = 0;
-        int sOdd = 0;
-        int sEven = 0;
-        // Tổng các số tự nhiên không lớn hơn n
-        while (i < n) {
+        
+        System.out.println("Các số tự nhiên chẳn nhỏ hơn n: ");
+        for (int i = 0; i < n; i++) {
             if (i % 2 == 0) {
-                sEven += i;
-            } else {
-                sOdd += i;
+                System.out.print(i+"\t");
             }
-            i++;
         }
-        System.out.printf("\ntổng các số tự nhiên lẻ nhỏ hơn %d là %d", n, sOdd);
-        System.out.printf("\ntổng các số tự nhiên chẵn nhỏ hơn %d là %d", n, sEven);
+        System.out.println("\nCác số tự nhiên lẻ nhỏ hơn n: ");
+        for (int i = 0; i < n; i++) {
+            if(i%2!=0) {
+                System.out.print(i+"\t");
+            }
+        }
     }
 }
