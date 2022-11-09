d3d78ab54d27fe2d46dee0e9fdad5b9373edf176
nguyenntt <nt.thaonguyen25@gmail.com>
2022-11-09 13:39:23 +0700

    Add HAS-I Relation file

---
+++ Lesson06/src/Exercise01/BookBorrowManagement.java
@@ -0,0 +1,6 @@
+package Exercise01;
+
+public class BookBorrowManagement {
+    private Reader[] readers;
+    private Book[] books;
+}
--- Lesson06/src/Exercise02/Score.java
+++ Lesson06/src/Exercise02/Score.java
@@ -1,4 +1,6 @@
 package Exercise02;
 
 public class Score {
+    private Student[] students;
+    private Subject[] subjects;
 }
