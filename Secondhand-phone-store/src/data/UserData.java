d235e52907c4b791eb0fee30d8494bfcc5a7eb8b
nguyenntt <106979668+Nolongeruser9771@users.noreply.github.com>
2022-12-05 12:42:57 +0700

    add admin account

--- Secondhand-phone-store/src/data/UserData.java
+++ Secondhand-phone-store/src/data/UserData.java
@@ -7,11 +7,11 @@ import java.util.ArrayList;
 public class UserData {
     //Khởi tạo CSDL cho User
     public ArrayList<User> userData(ArrayList<User> users) {
-        User user1 = new User("Nguyên", "0704690014", "BD", "nt@gmail.com", "Nguyen", "Nguyen@97", "MEMBER");
-        User user2 = new User("Nguyên", "0704690014", "BD", "ntt@gmail.com", "NguyenNguyen", "Nguyen@97", "ADMIN");
+        User member1 = new User("Nguyên", "0704690014", "BD", "nt@gmail.com", "Nguyen", "Nguyen@97", "MEMBER");
+        User admin1 = new User("Nguyên", "0539174684", "BD", "nguyennt@gmail.com", "NguyenNguyen", "Nguyen@97", "ADMIN");
 
-        users.add(user1);
-        users.add(user2);
+        users.add(member1);
+        users.add(admin1);
         return users;
     }
 }
